d50b8c2236aefdb8c9360d5962be94067ca0dcde
Daniel Nachun <daniel.nachun@gmail.com>
2025-02-01 16:28:54 -0800

    fix builds

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,6 @@
 perl Makefile.PL INSTALLDIRS=vendor NO_PERLLOCAL=1 NO_PACKLIST=1
-make
-if [[ ${build_platform} != ${target_platform} ]]; then
+make CC=${CC}
+if [[ ${build_platform} == ${target_platform} ]]; then
     make test
 fi
 make install VERBINST=1
